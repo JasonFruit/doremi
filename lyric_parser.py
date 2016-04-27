5f8f8039337b0531ad9a773e1a398ae57741b0d7
Jason R. Fruit <JasonFruit@gmail.com>
2016-04-27 03:16:49 -0500

    Added some additional explanatory comments to the lyric parser and associated objects.

--- lyric_parser.py
+++ lyric_parser.py
@@ -1,11 +1,13 @@
+"""Provides objects to parse and represent words for tunes notated in
+the Doremi music-representation language"""
+
 import codecs
 from parsimonious import Grammar, NodeVisitor
 
 class Verse(object):
+    """Represents the words to a stanza of a lyric"""
     def __init__(self):
         self.words = []
-    def __repr__(self):
-        return " ".join(self.words)
     def __word_to_lilypond(self, word):
         try:
             w = int(word)
@@ -17,12 +19,11 @@ class Verse(object):
                          for word in self.words])
 
 class LyricVoice(object):
+    """Represents the association of a musical voice and the verses of
+words that are sung to it"""
     def __init__(self):
         self.name = ""
         self.verses = [Verse()]
-    def __repr__(self):
-        return """  name: %s
-  %s""" % (self.name, "\n  ".join([repr(v) for v in self.verses]))
     def to_lilypond(self):
         return "\n".join([r"""\new Lyrics \lyricsto "exportedvoice_%s"
         { 
@@ -32,18 +33,14 @@ class LyricVoice(object):
                           for i in range(len(self.verses))])
 
 class Lyric(object):
+    """Represents words to be sung, with special provision for strophic
+lyrics"""
     def __init__(self):
         self.title = ""
         self.author = ""
         self.meter = ""
         self.voices = [LyricVoice()]
-    def __repr__(self):
-        return """title: %s
-author: %s
-meter: %s
-voices: 
-%s""" % (self.title, self.author, self.meter, "\n".join([repr(v)
-                                                         for v in self.voices]))
+
     def to_lilypond(self):
         return "\n\n".join([voice.to_lilypond()
                             for voice in self.voices])
@@ -65,23 +62,31 @@ raise a ValueError"""
         raise ValueError("No string value.")
         
 class LyricParser(NodeVisitor):
+    """Parses .drmw lyric files for association with Doremi tunes"""
     def __init__(self, text):
         NodeVisitor.__init__(self)
 
+        # start with a new empty lyric
         self.lyric = Lyric()
+        # add an empty voice to it
         self.lyric.voices.append(LyricVoice())
-        
+
+        # build an abstract syntax tree
         self.grammar = Grammar(open("lyric-grammar", "r").read())
         self.syntax = self.grammar.parse(text)
         
     def convert(self):
+        """Convert the syntax tree to our internal representation"""
         self.visit(self.syntax)
 
+        # remove any extra empty voices
         self.lyric.voices = [voice for voice in self.lyric.voices
                              if voice.name != ""]
-        
+
+        # remove any extra empty verses
         for voice in self.lyric.voices:
             voice.verses = voice.verses[:-1]
+            
         return self.lyric
 
     def visit_title(self, node, vc):
@@ -94,12 +99,14 @@ class LyricParser(NodeVisitor):
         self.lyric.meter = get_string_val(node)
 
     def visit_voicespec(self, node, vc):
+        # the current voice is complete, so start a new one
         self.lyric.voices.append(LyricVoice())
         
     def visit_voice(self, node, vc):
         self.lyric.voices[-1].name = get_node_val(node, "name")
 
     def visit_verse(self, node, vc):
+        # the verse is complete, so start a new one
         self.lyric.voices[-1].verses.append(Verse())
 
     def visit_word(self, node, vc):
