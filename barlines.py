9d2a4b88d8e64df696823ab9b1537af54841d035
Jason R. Fruit <JasonFruit@gmail.com>
2016-04-30 14:05:34 -0500

    Added ability to handle minor keys, barlines, and spacing that adapts to the minimum note duration in a tune.

---
+++ barlines.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+durations = {"16": 1,
+             "8": 2,
+             "8.5": 3,
+             "4": 4,
+             "4.": 6,
+             "2": 8,
+             "2.": 12,
+             "1": 16,
+             "1.": 24,
+             "0": 0}
+
+def measure_duration(time_sig):
+    num, denom = [int(part)
+                  for part in time_sig.split("/")]
+    return int(num * 16 / denom)
+
+class DurationCounter(object):
+    def __init__(self, time_sig, partial):
+        self.measure_dur = measure_duration(time_sig)
+        self.cur = self.measure_dur - durations[partial]
+    def at_barline(self):
+        return (self.cur % self.measure_dur) == 0
+    def count(self, dur):
+        self.cur += durations[dur]
+        return self.cur
--- doremi_canvas.py
+++ doremi_canvas.py
@@ -4,6 +4,7 @@ import math
 
 import gtk
 from doremi.doremi_parser import DoremiParser, Note
+from barlines import DurationCounter, durations
 
 # the scale degree number for each syllable in major and minor
 pitch_level = {"major": {"do": 0,
@@ -34,12 +35,15 @@ for pitch in pitch_level["major"].keys():
         minor_level = level - 5
     pitch_level["minor"][pitch] = minor_level
 
-    
 class DoremiCanvas(gtk.DrawingArea):
     """A GTK control to display a Doremi voice"""
-    def __init__(self, voice, octave_offset = 0):
+    def __init__(self, tune, voice, octave_offset = 0):
         gtk.DrawingArea.__init__(self)
-        self.voice = voice
+        self.dc = DurationCounter(tune.time, str(tune.partial))
+        self.tune = tune
+        self.voice = [v
+                      for v in tune
+                      if v.name == voice][0]
         self.set_size_request(200, 200)
         self.show_all()
         self.default_gc = None
@@ -47,11 +51,22 @@ class DoremiCanvas(gtk.DrawingArea):
         self.vspace = 7
         self.stem_length = self.vspace * 3
         self.octave_offset = octave_offset
+
+        self.min_dur = 24
+
+        for note in self.voice:
+            try:
+                dur = durations[note.duration]
+                if dur < self.min_dur:
+                    self.min_dur = dur
+            except AttributeError:
+                pass # ignore non-notes
+
+        self.sxt_space = math.ceil(15. / self.min_dur) + 1
+        
     def get_gc(self):
         """Get a basic graphic context"""
-        if self.default_gc:
-            return self.default_gc
-        else:
+        if not self.default_gc:
             drawable = self.window
             self.default_gc = drawable.new_gc(foreground=None,
                                               background=None,
@@ -71,7 +86,7 @@ class DoremiCanvas(gtk.DrawingArea):
                                               line_style=-1,
                                               cap_style=-1,
                                               join_style=-1)
-            return self.default_gc
+        return self.default_gc
         
     def draw_staff(self):
         """Draw five lines across the control, leaving room top and bottom for
@@ -87,19 +102,39 @@ additional notes"""
             self.bottom_line = y
             lnum += 1
 
+    def draw_ledger_lines(self, x, num):
+        drawable = self.window
+        gc = self.get_gc()
+        
+        if num < 0:
+            r = range(num, 0)
+        else:
+            r = range(1, num + 1)
+
+        for i in r:
+            y = self.bottom_line + (2 * i * self.vspace)
+            drawable.draw_line(gc,
+                               x - self.vspace - 2, y,
+                               x + self.vspace + 2, y)
+
     def draw_note(self, note, duration, octave, key, x):
         """Draw a note of a given scale degree, duration, etc."""
         drawable = self.window
         gc = self.get_gc()
+
+        try:
+            dur_num = int(duration)
+        except ValueError:
+            dur_num = float("%s5" % duration)
 
         note_num = pitch_level[key][note]
 
         octave += self.octave_offset
 
         # is the notehead filled?  is it dotted? does it have a stem? which way?
-        filled = (math.floor(duration) > 2)
-        dotted = type(duration) != int
-        has_stem = (math.floor(duration) > 1)
+        filled = (math.floor(dur_num) > 2)
+        dotted = type(dur_num) != int
+        has_stem = (math.floor(dur_num) > 1)
         
         if note_num + (7 * octave) > 3:
             stem_dir = 1
@@ -193,14 +228,14 @@ additional notes"""
             drawable.draw_rectangle(gc,
                                     filled,
                                     x - self.vspace,
-                                    y - self.vspace,
+                                    y - self.vspace + 1,
                                     self.vspace * 2,
-                                    self.vspace * 2)
+                                    self.vspace * 2 - 2)
 
             if has_stem:
                 drawable.draw_line(gc,
                                    x - self.vspace * stem_dir,
-                                   y + self.vspace * stem_dir,
+                                   y + self.vspace * stem_dir - stem_dir,
                                    x - self.vspace * stem_dir,
                                    y + self.vspace * stem_dir + self.stem_length * stem_dir)                                   
             
@@ -237,51 +272,41 @@ additional notes"""
                                   5, 5,
                                   0, 360 * 64)
 
+        ledger_lines = 0 - int(math.floor((note_num + (octave * 7)) / 2))
+        self.draw_ledger_lines(x, ledger_lines)
+
+        self.dc.count(str(duration))
+
+        space_after = self.sxt_space * (16 / dur_num)
+        
+        if self.dc.at_barline():
+            self.draw_barline(x + space_after / 2)
+
+        return x + space_after
+
+    def draw_barline(self, x):
+        gc = self.get_gc()
+        drawable = self.window
+        drawable.draw_line(gc,
+                           int(x), self.y_offset,
+                           int(x), self.bottom_line)
+        
     def draw(self):
+        self.draw_staff()
+        x = 15
         for note in self.voice:
             if type(note) == Note:
-                try:
-                    duration = int(note.duration)
-                except ValueError:
-                    duration = float("%s5" % note.duration)
-
-                self.draw_note(note.pitch,
-                               duration,
-                               note.octave,
-                               "major",
-                               x)
-                x += (15 * (16 / duration))
+                x = self.draw_note(note.pitch,
+                                   note.duration,
+                                   note.octave,
+                                   self.tune.key.split(" ")[1],
+                                   x)
+                
             else:
                 pass #handle non-notes here
             
-    def on_btn_press(self, btn):
-        self.draw_staff()
-
-        x = 25
-
-        for note in ["do", "re", "mi", "fa", "sol", "la", "ti"]:
-            self.draw_note(note,
-                           4,
-                           -1,
-                           "major",
-                           x)
-            x += 20
-
-        for note in ["do", "re", "mi", "fa", "sol", "la", "ti"]:
-            self.draw_note(note,
-                           4,
-                           0,
-                           "major",
-                           x)
-            x += 20
-
-        for note in ["do", "re", "mi", "fa", "sol", "la", "ti"]:
-            self.draw_note(note,
-                           4,
-                           1,
-                           "major",
-                           x)
-            x += 20
+    def on_btn_press(self, btn, *args):
+        self.draw()
 
 if __name__ == "__main__":
     win = gtk.Window()
@@ -294,7 +319,7 @@ if __name__ == "__main__":
     dp = DoremiParser("tunes/goble.drm")
     tune = dp.convert()
     
-    dc = DoremiCanvas(tune[0])
+    dc = DoremiCanvas(tune, tune[0].name)
 
     vbx.pack_start(dc, True)
 
